fafe2cf0cdc8bbb23d68e4d97b7175f35b20a35a
modar4282 <modar4282@gmail.com>
2025-04-30 17:01:38 +0300

    updated title

--- src/Pages/Customer/Helper/index.tsx
+++ src/Pages/Customer/Helper/index.tsx
@@ -2,34 +2,38 @@ import qouteP from "../../../Assets/images/quoteP.png";
 import qouteB from "../../../Assets/images/quoteB.png";
 export const CardArray = [
   {
-    url: 'https://thumbs.dreamstime.com/b/office-smile-portrait-businessman-tablet-online-schedule-connection-planning-networking-communication-happy-man-344894855.jpg',
+    // url: 'https://thumbs.dreamstime.com/b/office-smile-portrait-businessman-tablet-online-schedule-connection-planning-networking-communication-happy-man-344894855.jpg',
     quote: qouteB,
+    url:'',
     color: "#B238E138",
-    titleCard: "عمر ",
+    titleCard:'',// "عمر ",
     bodyCard:
       "ARAK+ ساعدتني بشكل كبير في استخراج التراخيص وتأسيس مشروعي في وقت قياسي. فريقهم محترف ويعرف السوق المحلي جيدًا.",
   },
   {
-    url: 'https://img.freepik.com/free-photo/portrait-smiling-plus-size-woman_23-2150165156.jpg?semt=ais_hybrid&w=740',
+    // url: 'https://img.freepik.com/free-photo/portrait-smiling-plus-size-woman_23-2150165156.jpg?semt=ais_hybrid&w=740',
     quote: qouteP,
+    url:'',
     color: "#0045B24D",
-    titleCard: "ريم",
+    titleCard:'',// "ريم",
     bodyCard:
       "تعاملت مع ARAK+ للحصول على تأشيرة إقامة طويلة الأمد، وكانت الخدمة ممتازة وسريعة. أوصي بهم لأي مستثمر جديد في سوريا.",
   },
   {
-    url: 'https://cdn08.nnnow.com/web-images/large/styles/5G632OC1MC0/1666263424873/1.jpg',
+    // url: 'https://cdn08.nnnow.com/web-images/large/styles/5G632OC1MC0/1666263424873/1.jpg',
     quote: qouteB,
+    url:'',
     color: "#B238E138",
-    titleCard: "فادي ",
+    titleCard:'',// "فادي ",
     bodyCard:
       "بفضل دعم ARAK+، تمكنت من دراسة جدوى مشروعي وفهم التكاليف والمخاطر بدقة. خبرتهم في السوق لا تقدر بثمن.",
   },
   {
-    url: 'https://www.arabiaweddings.com/sites/default/files/styles/max980/public/listing/2020/04/29/round-face_1.jpg?itok=C53-mPz6',
+    // url: 'https://www.arabiaweddings.com/sites/default/files/styles/max980/public/listing/2020/04/29/round-face_1.jpg?itok=C53-mPz6',
     quote: qouteP,
+    url:'',
     color: "#0045B24D",
-    titleCard: "ليلى ",
+    titleCard:'',// "ليلى ",
     bodyCard:
       "خدمات ARAK+ اللوجستية والتنظيمية وفرت علينا الكثير من الوقت والجهد. كل شيء كان منظمًا ومهنيًا للغاية.",
   },
